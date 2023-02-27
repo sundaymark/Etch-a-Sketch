b4699f83a5c759a12335da25f112e0674936a448
sundaymark <sundayukpai1122@gmail.com>
2023-02-27 12:33:57 +0100

    craeted the random function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let color = "black";
 function populateBoard(size) {
   const board = document.querySelector(".board");
   board.style.gridTemplateColumns = `repeat(${size},1fr)`;
@@ -5,10 +6,7 @@ function populateBoard(size) {
 
   for (let i = 0; i < 256; i++) {
     let boardAdd = document.createElement("div");
-    boardAdd.addEventListener("mouseover", () => {
-      boardAdd.style.backgroundColor = "black";
-      
-    });
+    boardAdd.addEventListener("mouseover", colorSqare);
     boardAdd.style.backgroundColor = "blue";
     board.appendChild(boardAdd);
   }
@@ -19,4 +17,15 @@ function changeSize(input) {
   if (input >= 2 && input <= 40) {
     populateBoard(input);
   } else alert("input must be between 2 and 40");
+}
+
+function colorSqare() {
+  if (color == "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = "color";
+  }
+}
+function changeColor(choice) {
+  color = choice;
 }
